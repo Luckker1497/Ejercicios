fa8a827111eba8515944f04b2f10d141b68f3a63
Luckker <lucker1497@gmail.com>
2024-08-12 16:16:28 -0600

    Agregue elementos al ejercicio y se da por terminado el ejercicio

---
+++ Ejercicio1 Sintaxis.py
@@ -0,0 +1,31 @@
+
+#https://www.python.org/
+
+#Comentario en una linea
+
+"""
+Esto tambien es una sentencia de comentarios,
+todo lo que se guarde dentro de estas comillas 
+sirven como comentario
+"""
+#Declaracion de variables
+my_variable  = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+#Constantes
+MY_CONSTANTE = "Mi constante"  #Marcar siempre que la variable es constante
+
+#Tipos de datos primitivos
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_str = "Mi cadena de texto"
+
+#Imprimir un dato
+print("Hola, Python")
+
+#Imprime el tipo de dato que pertenece el elemento de la variable
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_str))
