839e5067d8337d0a33622ddc8e5e855eb4ece71a
Luckker <lucker1497@gmail.com>
2024-08-12 13:59:49 -0600

    Agregue elementos al ejercicio

---
+++ Ejercicio 1.py
@@ -0,0 +1,18 @@
+
+
+
+#https://www.python.org/
+
+#Comentario en una linea
+
+"""
+Esto tambien es una sentencia de comentarios,
+todo lo que se guarde dentro de estas comillas 
+sirven como comentario
+"""
+
+my_variable  = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANTE = "Mi constante"  #Marcar siempre que la variable es constante
+
