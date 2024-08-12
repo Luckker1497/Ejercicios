6a14a62a64d598a05f1c73e4151b3a19141acc33
Luckker <lucker1497@gmail.com>
2024-08-12 15:41:18 -0600

    Agregue elementos al ejercicio

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,6 +1,3 @@
-
-
-
 #https://www.python.org/
 
 #Comentario en una linea
@@ -10,9 +7,18 @@ Esto tambien es una sentencia de comentarios,
 todo lo que se guarde dentro de estas comillas 
 sirven como comentario
 """
-
+#Declaracion de variables
 my_variable  = "Mi variable"
 my_variable = "Nuevo valor de mi variable"
-
+#Constantes
 MY_CONSTANTE = "Mi constante"  #Marcar siempre que la variable es constante
+
+#Tipos de datos primitivos
+my_int = 1 #enteros
+my_float = 1.5 #decimales 
+my_bool = True #booleano
+my_bool = False
+my_str = "Mi cadena de texto" #Texto
+
+#print("¡Hola Python!")
 
